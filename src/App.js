67c6b3bbdd79eb853be57e58e7772e0884d3bd04
J. Cohen <hostelmarketing@gmail.com>
2020-03-19 13:22:31 -0700

    Add homepage component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./homepage.component";
+
 function App() {
     return (
         <div className="App">
-            Hello world
+            <HomePage />
         </div>
     );
 }
