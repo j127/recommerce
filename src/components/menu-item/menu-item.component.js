ee49b393120981e93e0851d9df3f614dfe1cfaa7
J. Cohen <hostelmarketing@gmail.com>
2020-03-20 16:56:09 -0700

    Finish section 4

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -3,10 +3,11 @@ import "./menu-item.styles.scss";
 
 export default function MenuItem({ title, imageUrl, size }) {
     return (
-        <div
-            className={`${size} menu-item`}
-            style={{ backgroundImage: `url(${imageUrl})` }}
-        >
+        <div className={`${size} menu-item`}>
+            <div
+                className="background-image"
+                style={{ backgroundImage: `url(${imageUrl})` }}
+            />
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">Shop Now</span>
