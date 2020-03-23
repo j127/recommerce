4e28309d8cefc5d7b5657f1e2f75360c9f175d52
J. Cohen <hostelmarketing@gmail.com>
2020-03-22 20:13:06 -0700

    Add sign up form for login with email and password

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends React.Component {
                         },
                         () => console.log(this.state)
                     );
+                    console.log("x", this.state);
                 });
             }
 
