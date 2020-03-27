a9e93f54bd084c017608cca236163746e75f7a71
J. Cohen <hostelmarketing@gmail.com>
2020-03-27 13:20:33 -0700

    Add reselect for memoized selectors in the cart.

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,30 @@
+import { createSelector } from "reselect";
+
+// There are two types of selectors to think about:
+// - input selector (doesn't use `createSelector`)
+// - ouput selector (uses `createSelector`)
+
+// gets the entire state and slices off the needed bit (the cart)
+const selectCart = state => state.cart;
+
+// This makes each selector a memoized selector. `createSelector` takes
+// an array of selectors and passes each of them as arguments into the
+// function that makes up the second argument. For example, you could do:
+// ```
+// createSelector([selectCart, selectUser], (cart, user) => ...
+// ```
+// which would return the output of those selectors.
+export const selectCartItems = createSelector(
+    [selectCart], // the array of selectors
+    cart => cart.cartItems // a function that takes each selector as an arg
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumulatedQuantity, cartItem) =>
+                accumulatedQuantity + cartItem.quantity,
+            0
+        )
+);
