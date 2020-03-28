27b66d59216980848ffb0c99ea5669b7e865b484
J. Cohen <hostelmarketing@gmail.com>
2020-03-28 16:00:06 -0700

    Add redux-persist to save data in localStorage.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+
+// This tells it to use localStorage as the default storage (instead of
+// session storage).
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+    key: "root",
+    storage, // use localStorage, see above
+    whitelist: ["cart"], // a list of reducers to persist
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
